e02ed853e958a4625515b7c6f51ff313eb1b1e2c
Ajoy34 <ajoyshutradhar53@gmail.com>
2026-07-22 23:58:02 +0600

    Deploy Discounty static web app

---
+++ _next/static/sk3Q2u0VOSZD-jrj-xAf0/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[locale]","\u002F[locale]\u002Fadmin","\u002F[locale]\u002Fdashboard","\u002F[locale]\u002Flogin","\u002F[locale]\u002Foffers","\u002F[locale]\u002Fsearch","\u002F[locale]\u002Fshop\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
